ac295b7f62b5a47d49fe158962787edecbb0c3ba
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-06 09:42:49 -0500

    test for test

--- Deliverables/2/2.1/tests.py
+++ Deliverables/2/2.1/tests.py
@@ -1,5 +1,6 @@
 import unittest
 import BackEndComponent
+import json
 
 def test1():
 	special_obj = json.load(sys.stdin)
@@ -8,3 +9,13 @@ def test1():
 	sorted_ten = back_service.sort(special_obj)
 
 	sys.stdout.write(sorted_ten[0])
+
+def testOne():
+	with open('input1.json') as file:
+		input = json.load(file)
+	with open('output1.json') as file:
+		output = json.load(file)
+
+	back_service = BackEndComponent()
+	sorted_ten = back_service.sort(input)
+	assert(sorted_ten == output)
