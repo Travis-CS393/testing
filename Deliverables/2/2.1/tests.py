26a6e88260b5bfa45b7cea8f2f534457f524fbe3
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-07 10:17:32 -0500

    strings only

--- Deliverables/2/2.1/tests.py
+++ Deliverables/2/2.1/tests.py
@@ -73,7 +73,7 @@ def test_driver():
 	back_service = BackEndComponent()
 	# sorted_ten = back_service.sort(special_obj)
 	a, b, c = back_service.sort(special_obj)
-	print(a + b + c)
+	print(b)
 
 	# print(sorted_ten)
 
