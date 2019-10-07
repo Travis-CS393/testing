0600e2749d42da3631c82d2d5097ab4e5a569f81
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-07 02:21:32 -0500

    dumps

--- Deliverables/2/2.1/backend.py
+++ Deliverables/2/2.1/backend.py
@@ -51,6 +51,7 @@ class BackEndComponent():
 		# return sorted(num_lst) + sorted(str_lst) + self.obj_sort(obj_lst)
 		num = sorted(num_lst)
 		stri = sorted(str_lst)
-		slist = [x.encode('UTF8') for x in stri]
+		# slist = [x.encode('UTF8') for x in stri]
+		slist = json.dumps(stri)
 		obj = self.obj_sort(obj_lst)
 		return slist
