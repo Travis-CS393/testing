3786663d402ebd29bd29f0e03c31763967fc593a
JennyTL8 <jennytl8@github.com>
2019-10-07 10:33:58 -0500

    change stdout

--- Deliverables/2/2.1/tests.py
+++ Deliverables/2/2.1/tests.py
@@ -108,6 +108,7 @@ def test_driver():
 	back_service = BackEndComponent()
 	sorted_ten = back_service.sort(special_obj)
 
-	print(sorted_ten)
+	#print(sorted_ten)
+	sys.stdout.write(sorted_ten)
 	
 test_driver()
