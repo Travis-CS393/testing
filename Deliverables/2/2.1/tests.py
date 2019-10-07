97a380442239f154b5b3e542b1a4e5180d9dd276
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-07 10:52:03 -0500

    appending one at a time

--- Deliverables/2/2.1/tests.py
+++ Deliverables/2/2.1/tests.py
@@ -74,7 +74,9 @@ def test_driver():
 	back_service = BackEndComponent()
 	# sorted_ten = back_service.sort(special_obj)
 	a, b, c = back_service.sort(special_obj)
-	print(c)
+	for bs in b:
+		a.append(bs)
+	print(a)
 
 	# print(sorted_ten)
 """
