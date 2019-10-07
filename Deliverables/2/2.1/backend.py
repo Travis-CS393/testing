6aa949aa2141cd7f47468d7e989224223f5cdff0
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-07 02:17:27 -0500

    two

--- Deliverables/2/2.1/backend.py
+++ Deliverables/2/2.1/backend.py
@@ -51,6 +51,6 @@ class BackEndComponent():
 		# return sorted(num_lst) + sorted(str_lst) + self.obj_sort(obj_lst)
 		num = sorted(num_lst)
 		stri = sorted(str_lst)
-		slist = [x.encode('UTF8') for x in slist]
+		slist = [x.encode('UTF8') for x in stri]
 		obj = self.obj_sort(obj_lst)
 		return slist
