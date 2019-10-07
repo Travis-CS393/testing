f045e48afcaff8d2a4f0ae7458123faddfc21e95
JennyTL8 <jennytl8@github.com>
2019-10-06 23:02:51 -0500

    All the type errors

--- Deliverables/2/2.1/backend.py
+++ Deliverables/2/2.1/backend.py
@@ -40,9 +40,9 @@ class BackEndComponent():
 			if isinstance(s_obj, int):
 				num_lst.append(s_obj)
 			elif (isinstance(s_obj, str) or isinstance(s_obj, unicode)):
-				str_lst.append(s_obj)
+				str_lst.append(str(s_obj))
 			elif isinstance(s_obj, dict):
-				obj_lst.append(s_obj)
+				obj_lst.append(json.dumps(s_obj))
 			else:
 				print("Type Error: Special JSON object is one of int, str, or JSON obj")
 				print(type(s_obj))
