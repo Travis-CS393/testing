343e71bd6fabcde807708e137f14f3c6fa919870
JennyTL8 <jennytl8@github.com>
2019-10-08 04:13:59 -0500

    add

--- Deliverables/2/2.2/frontend.py
+++ Deliverables/2/2.2/frontend.py
@@ -76,3 +76,29 @@ class FrontEndComponent():
 
 		#sys.stdout.write(sorted_lsts)
 		print(sorted_lsts)
+
+	def test_driver():
+		sorted_lsts = []
+		temp = ""
+		hold = sys.stdin.readlines()
+		for line in hold:			
+			line = line.replace("\n","")
+			try:
+				while(line):
+					data, idx = json.JSONDecoder().raw_decode(temp + line)
+					self.lst.append(data)
+					if (idx == len(line)):
+						break
+					temp = temp[idx:]
+					line = line[idx:]
+				except ValueError:
+					temp = temp + line
+
+		partitioned_lst = self.partition(self.lst)
+		back_service = BackEndComponent()
+		for lst in partitioned_lst:
+			sorted_lsts.append(back_service.sort(lst))
+
+		print(sorted_lsts, end='')
+
+test_driver()
