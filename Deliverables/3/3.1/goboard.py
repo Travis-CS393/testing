84d46e658e1ae2ffdb215b5b7778589822edc82d
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-14 00:15:26 -0500

    testing with queue

--- Deliverables/3/3.1/goboard.py
+++ Deliverables/3/3.1/goboard.py
@@ -197,6 +197,7 @@ class GoBoardComponent():
 		marks = [ [False] * 19 for row in range(19)]
 		# if maybe_stone is same as point, then return True
 		if (self.go_board[x][y] == maybe_stone):
+			print("entered")
 			return True
 		q = Queue.Queue()
 		neighbors = self.findNeighbors(point)
