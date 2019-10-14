264d0050eb35f21ced86bb2788c66caaa2fe4923
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-13 23:56:38 -0500

    added the difference check

--- Deliverables/3/3.1/goboard.py
+++ Deliverables/3/3.1/goboard.py
@@ -151,8 +151,9 @@ class GoBoardComponent():
 			px, py = self.process_point(a)
 			diffx = abs(px - x)
 			diffy = abs(py - y)
-			str_point = str(px) + "-" + str(py)
-			connected.append(str_point)
+			if (diffx <= 1 ^ diffy <= 1):
+				str_point = str(px) + "-" + str(py)
+				connected.append(str_point)
 		return connected
 
 	def findNeighbors(self, point):
