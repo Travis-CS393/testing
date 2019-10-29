0041a0383e989c39eb7e9192bbce189633fcc92b
Bella Zhan <bellazhan2021@u.northwestern.edu>
2019-10-29 04:22:06 -0500

    the final maybe

--- Deliverables/5/5.1/test_driver.py
+++ Deliverables/5/5.1/test_driver.py
@@ -2,12 +2,11 @@ from go import GoBoard, GoPlayerMin
 import json
 import sys
 
-# question 5
 ##########################################
 # HELPER FUNCTIONS
 ##########################################
 
-# Reads and returns array of [Board, Statement] JSON array elements from STDIN 
+# Reads and returns array of [Board, Statement] JSON array elements from STDIN
 def read_input():
 	inputs = []
 	reading_frame = ""
@@ -32,7 +31,7 @@ def print_output(output):
 # TYPE ASSERTIONS PREDICATES
 ###########################################
 
-# Checks that input is one of the valid types of input 
+# Checks that input is one of the valid types of input
 # ["register"]
 # ["receive-stones", Stone]
 # ["make-a-move", Boards]
@@ -55,7 +54,7 @@ def check_stone(stone):
 def check_maybe_stone(maybe_stone):
 	return (check_stone(maybe_stone) or (maybe_stone == " "))
 
-# Board must be 19 x 19 represented by an array of 19 rows 
+# Board must be 19 x 19 represented by an array of 19 rows
 # where each row contains 19 elements of type MaybeStone
 def check_board(board, board_size):
 	check_all_stones = True
@@ -87,7 +86,3 @@ def test_driver():
 	print(print_output(outputs))
 
 test_driver()
-
-
-
-
