e44f0c6e258e14d450eb3b9008d48ed76888c292
JennyTL8 <jennytl8@github.com>
2019-10-30 20:44:39 -0500

    why

--- Deliverables/5/5.2/go.py
+++ Deliverables/5/5.2/go.py
@@ -59,8 +59,8 @@ class GoPlayerCapture():
 	# Makes a legal move if possible, given board history 
 	def make_move(self, history):
 		board_checker = GoBoard()
-		#keep_history = copy.deepcopy(history)
-		if (board_checker.validate_history(self.player_stone, history)):
+		keep_history = copy.deepcopy(keep_history)
+		if (board_checker.validate_history(self.player_stone, keep_history)):
 			return self.find_capture(self.player_stone, history)
 		else:
 			return "This history makes no sense!"
